ad857212505b493449d301590088fd2ebf0628d6
Adithya Nair <adityasnair2000@gmail.com>
2026-01-05 15:11:11 -0800

    Built homepage

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,12 @@
 import { SanityLive } from '@/sanity/lib/live'
 import { ClerkProvider } from '@clerk/nextjs'
 import React from 'react'
+import { AppHeader } from './app/layout/AppHeader'
 
 function AppLayout({children} : {children: React.ReactNode}) {
   return (
     <ClerkProvider>
+        <AppHeader />
         {children}
         <SanityLive />
     </ClerkProvider>
