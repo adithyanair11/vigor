67b82abc6cc7fd2bc0ae396231c076f1cdaa64c8
Adithya Nair <adityasnair2000@gmail.com>
2026-01-07 21:27:20 -0800

    Added sanity app sdk

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,7 @@
 import { AppHeader } from "@/components/app/layout/AppHeader";
+import { AppShell } from "@/components/app/layout/AppShell";
 import { OnboardingGuard } from "@/components/app/onboarding/OnBoardingGaurd";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
@@ -7,11 +9,15 @@ import React from "react";
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <OnboardingGuard>
-        <AppHeader />
-        {children}
-      </OnboardingGuard>
-      <SanityLive />
+      <ChatStoreProvider>
+        <OnboardingGuard>
+          <AppShell>
+            <AppHeader /> 
+            {children}
+          </AppShell>
+        </OnboardingGuard>
+        <SanityLive />
+      </ChatStoreProvider>
     </ClerkProvider>
   );
 }
